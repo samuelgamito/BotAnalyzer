0f9f461086ddbc273c77c1fbac68c9a600efcced
Samuel Gamito <samuel.gamito@ab-inbev.com>
2020-11-02 11:05:59 -0300

    Add build script

---
+++ build_dashboard_api.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+
+BASE_PATH=`pwd`
+TARGET_PATH="${BASE_PATH}/target/dashboard_api"
+PROJECT_PATH="${BASE_PATH}/core/dashboard_api"
+
+echo "[Building] dashbord_api"
+echo "Project path: ${PROJECT_PATH}"
+echo "Target path: ${BASE_PATH}"
+
+rm -rf $TARGET_PATH
+
+mkdir -p $TARGET_PATH
+
+
+echo "Building NODE project dashboard_api"
+cd $PROJECT_PATH
+npm install
+npm run build
+
+
+echo "Moving project to target folder"
+mv -f build/ $TARGET_PATH
+cp -f package.json $TARGET_PATH
+
+
+echo "Download and packaging production dependecies"
+cd $TARGET_PATH
+npm install --only=prod
+tar -czf dashboard_api.tar.gz *
